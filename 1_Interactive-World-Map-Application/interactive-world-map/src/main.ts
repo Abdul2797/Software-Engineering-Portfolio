a6dcc47fcc5a8febde2f946a7de9a4ccdb56de9b
Abdul Siddiq <155837936+Abdul2797@users.noreply.github.com>
2025-12-18 21:04:41 -0500

    Add files via upload

---
+++ 1_Interactive-World-Map-Application/interactive-world-map/src/main.ts
@@ -0,0 +1,13 @@
+// main.ts
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()  //provides HttpClient to all standalone components
+  ]
+}).catch(err => console.error(err));
